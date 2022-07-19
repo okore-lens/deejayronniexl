75fd62b8431c39defc0a807754a716e995d25dbc
Infiinite-lens <okorelens27@gmail.com>
2022-07-19 15:48:10 +0300

    Worked on the correct routing of the application

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-import Nav_Bar from "./components/navbar/Nav_Bar";
+import NavBar from "./components/navbar/NavBar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/home/Home";
 import Merch from "./components/pages/merch/Merch";
@@ -13,13 +13,13 @@ function App() {
       <Container>
         <Row>
           <Col>
-            <Nav_Bar></Nav_Bar>
+            <NavBar></NavBar>
             <Routes>
-              <Route exact path="/" component={<Home />} />
-              <Route exact path="/merch" component={Merch} />
-              <Route exact path="/discography" component={Discography} />
-              <Route exact path="/contact-us" component={ContactUs} />
-              <Route exact path="/events" component={Events} />
+              <Route exact path="/" element ={<Home />} />
+              <Route exact path="/merch" element ={<Merch />} />
+              <Route exact path="/discography" element ={<Discography />} />
+              <Route exact path="/contact-us" element ={<ContactUs />} />
+              <Route exact path="/events" element ={<Events />} />
             </Routes>
           </Col>
         </Row>
