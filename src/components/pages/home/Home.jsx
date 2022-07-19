1172a54324455ee6af0d84ec64a41ed5eb690d34
Infiinite-lens <okorelens27@gmail.com>
2022-07-19 15:58:51 +0300

    Home NotePoints

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import './Home.scss'
+import React from "react";
+import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
-  )
-}
+    <div className="home">
+      Home
+      {/* Image Area --> Should cover at least 0.75 of the initial viewheight of the browser */}
+      {/* Engagement Area --> Has two areas the projects slide(covers at least 0.75 width of the section) and the feed section(where Twitter/Instagram Engagements are shown)*/}
+      {/* About Section --> A card that has a picture,text and social media links */}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
