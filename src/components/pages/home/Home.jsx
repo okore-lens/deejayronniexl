efd551a3120b55f172bbdc698a0037fd30341438
Infiinite-lens <okorelens27@gmail.com>
2022-07-20 09:34:52 +0300

    Home Page Development

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import "./Home.scss";
+import image from "../../../assets/images/img.jpg";
+import Container from "react-bootstrap/esm/Container";
 
 const Home = () => {
+  const myStyle = {
+    backgroundImage: `url(${image})`,
+  };
   return (
     <div className="home">
-      Home
       {/* Image Area --> Should cover at least 0.75 of the initial viewheight of the browser */}
+      <div style={myStyle} className="image"></div>
       {/* Engagement Area --> Has two areas the projects slide(covers at least 0.75 width of the section) and the feed section(where Twitter/Instagram Engagements are shown)*/}
+
+      <Container className="home-tile">Container</Container>
+
       {/* About Section --> A card that has a picture,text and social media links */}
     </div>
   );
