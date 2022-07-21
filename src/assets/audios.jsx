1263dce3dab5a2861da7745c7dbc2e098a08c05e
Infiinite-lens <okorelens27@gmail.com>
2022-07-21 16:54:10 +0300

    AudioArray

--- src/assets/audios.jsx
+++ src/assets/audios.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
-const audios = () => {
+const audios = (props) => {
   const audioArray = [
     {
-      title: <p>Audio Title --&gt; adipiscing elit, sed do eiusmod tempor </p>,
+      title: <p>Audio Title1 --&gt; adipiscing elit, sed do eiusmod tempor </p>,
       link,
     },
   ];
